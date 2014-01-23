275e13ea188abae6cc62e14a223feaf88c0b77d4
Guo Yu <i@guoyu.me>
2014-01-23 11:46:33 +0800

    0.0.1: update tpls

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
 var $ = require('dom'),
+    router = require('angular-ui-router'),
+    store = require('angular-store'),
     template = require('./template');
 
-var messager = function(trigger) {
-    if (!angular) return false;
+var messager = function(trigger, apis) {
+    if (!angular) return false
     if (!trigger) return false
     if (!$(trigger).length) return false
+    if (apis) this.apis = apis
+    this.deps = [router]
     this.trigger = $(trigger)
     this.init()
 }
 
-messager.prototype.init = function() {
-    console.log(123)
-    this.trigger.html(template);
-    this.ctrlers(angular.module('messager', ['store']));
-}
-
 messager.prototype.ctrlers = function(app) {
     if (!app) return false;
-    app.controller('home', function($scope, Store) {
-        $scope.name = 'fetch'
-    });
+    app.controller('contacts', function($scope, Store) {
+        console.log(Store);
+        $scope.contacts = [{
+            name: '小花',
+            avatar: 'http://pic.qiushibaike.com/system/avtnew/909/9099065/thumb/20131004173901.jpg'
+        },{
+            name: '小花',
+            avatar: 'http://pic.qiushibaike.com/system/avtnew/909/9099065/thumb/20131004173901.jpg'
+        },{
+            name: '小花',
+            avatar: 'http://pic.qiushibaike.com/system/avtnew/909/9099065/thumb/20131004173901.jpg'
+        },{
+            name: '小花',
+            avatar: 'http://pic.qiushibaike.com/system/avtnew/909/9099065/thumb/20131004173901.jpg'
+        },{
+            name: '小花',
+            avatar: 'http://pic.qiushibaike.com/system/avtnew/909/9099065/thumb/20131004173901.jpg'
+        },{
+            name: '小花',
+            avatar: 'http://pic.qiushibaike.com/system/avtnew/909/9099065/thumb/20131004173901.jpg'
+        }]
+    })
+}
+
+messager.prototype.store = function() {
+    if (!this.apis) return false
+    this.deps.push('store')
+    store.config(this.apis)
+}
+
+messager.prototype.init = function() {
+    if (this.apis) this.store()
+    this.trigger.html(template)
+    this.ctrlers(angular.module('messager', this.deps))
 }
 
 module.exports = messager
--- template.js
+++ template.js
@@ -1,1 +1,1 @@
-module.exports = '<div class="messager" ng-app="messager">\n    <h3>Messager</h3>\n    <div ng-controller="home">{{name}}</div>\n</div>';
+module.exports = '<div class="messager" ng-app="messager">\n    <h3 class="title">Messager</h3>\n    <div ng-controller="contacts">\n        <div class="contact clearfix" ng-repeat="contact in contacts">\n            <img ng-src="{{contact.avatar}}" alt="{{contact.name}}" class="pull-left">\n        </div>\n    </div>\n</div>';
